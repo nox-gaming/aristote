1475cbd662b2eef22bee6590930b2dbfb5b5a437
Ugo Arzur <ugo.arzur@gmail.com>
2019-04-13 18:35:49 +0200

    add new next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 // next.config.js
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const withSass = require('@zeit/next-sass')
+const { ANALYZE } = process.env
+
 module.exports = withSass({
-  cssModules: true
+  cssModules: true,
+  webpack: function (config) {
+    if (ANALYZE) {
+      config.plugins.push(new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerPort: 8888,
+        openAnalyzer: true
+      }))
+    }
+
+    return config
+  }
 })
